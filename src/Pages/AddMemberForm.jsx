9226bdb207f63f8902553a6892ea60df0eeea43e
sagar <shradhdhasagr1502@gmail>
2024-09-27 19:15:17 +0530

    all chnges completed

--- src/Pages/AddMemberForm.jsx
+++ src/Pages/AddMemberForm.jsx
@@ -3,120 +3,128 @@ import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import Select from "react-select";
 import NaccLogo from "../Images/NaccLogo.png";
 
-const RoleOptions = [
-    { value: "role", label: "Role" },
-    { value: "chapter1", label: "Chapter 1" },
-    { value: "chapter2", label: "Chapter 2" },
-    { value: "chapter3", label: "Chapter 3" },
-  ];
-  
-  const ChapterOptions = [
-    { value: "chapter", label: "Chapter" },
-    { value: "chapter1", label: "Chapter 1" },
-    { value: "chapter2", label: "Chapter 2" },
-    { value: "chapter3", label: "Chapter 3" },
-  ];
-  
-  const CommitteeOptions = [
-    { value: "committee", label: "Committee" },
-    { value: "committee1", label: "Committee 1" },
-    { value: "committee2", label: "Committee 2" },
-    { value: "committee3", label: "Committee 3" },
-  ];
-  
-  const customStyles = {
-    control: (base) => ({
-      ...base,
-      borderRadius: "21px",
-      padding: "2px",
-      backgroundColor: "#edefef",
-      borderColor: "transparent",
-      boxShadow: "none",
-      "&:hover": { borderColor: "gray" },
-    }),
-    placeholder: (base) => ({
-      ...base,
-      color: "#282728",
-      fontSize: "14px",
-    }),
-    singleValue: (base) => ({
-      ...base,
-      color: "#282728",
-      fontSize: "14px",
-    }),
-  };
+const roles = [
+  { value: 'admin', label: 'Admin' },
+  { value: 'user', label: 'User' },
+];
+
+const chapters = [
+  { value: 'chapter1', label: 'Chapter 1' },
+  { value: 'chapter2', label: 'Chapter 2' },
+];
+
+const committees = [
+  { value: 'committee1', label: 'Committee 1' },
+  { value: 'committee2', label: 'Committee 2' },
+];
+
+const customSelectStyles = {
+  control: (provided,state) => ({
+    ...provided,
+    borderRadius: '9999px', 
+    backgroundColor: '#edefef',
+    boxShadow: 'none', 
+    outline: 'none', 
+    borderColor: state.isFocused ? '#edefef' : '#edefef', 
+
+
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: '#282728', 
+    fontSize: "12px",
+    fontWeight: '400',
+    
+  }),
+
+  menu: (provided) => ({
+    ...provided,
+    
+  }),
+};
+
 
 const AddMemberForm = () => {
-    const [selectedRoleOption, setSelectedRoleOption] = useState(null);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    role: null,
+    chapter: null,
+    committee: null,
+  });
+  const handleChange = (selectedOption, field) => {
+    setFormData({ ...formData, [field]: selectedOption });
+  };
 
-    const handleChange = (option) => {
-      setSelectedRoleOption(option);
-    };
-    
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <div className="p-10 bg-[#ffffff] rounded-[50px] max-w-[350px] drop-shadow-[0px 5px 31px rgba(0,0,0,0.35)] w-full ">
-    <div className="flex justify-center mb-6">
-      <img src={NaccLogo} alt="nacc" className="items-center h-12" />
-    </div>
-    <form className="pt-5">
-      <input
-        type="text"
-        placeholder="First Name"
-        className="w-full px-4 py-2 mb-4 text-[#282728] placeholder:text-sm bg-[#edefef] rounded-[21px] focus:outline-none text-sm font-normal placeholder:text-[#282728]"
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        roleOption
-        className="w-full px-4 py-2 mb-4 text-[#282728] placeholder:text-sm bg-[#edefef] rounded-[21px] focus:outline-none text-sm font-normal placeholder:text-[#282728]"
-      />
-
-      <div className="mb-4">
-        <Select
-          options={RoleOptions}
-          defaultValue={RoleOptions[0]}
-          styles={customStyles}
-          components={{
-            DropdownIndicator: () => (
-              <ChevronDownIcon className="w-5 h-5 text-gray-400" />
-            ),
-          }}
-        />
+      <div className="flex justify-center mb-6">
+        <img src={NaccLogo} alt="nacc" className="items-center h-12" />
       </div>
-
-      <div className="mb-4 ">
-        <Select
-          options={ChapterOptions}
-          defaultValue={ChapterOptions[0]}
-          styles={customStyles}
-          components={{
-            DropdownIndicator: () => (
-              <ChevronDownIcon className="w-5 h-5 text-gray-400" />
-            ),
-          }}
+      <form className="pt-5">
+        <input
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleInputChange}
+          className="w-full px-4 py-2 mb-4 text-[#282728] placeholder:text-sm bg-[#edefef] rounded-[21px] focus:outline-none text-sm font-normal placeholder:text-[#282728]"
         />
-      </div>
-
-      <div className="mb-6 ">
-        <Select
-          options={CommitteeOptions}
-          defaultValue={CommitteeOptions[0]}
-          styles={customStyles}
-          components={{
-            DropdownIndicator: () => (
-              <ChevronDownIcon className="w-5 h-5 text-gray-400" />
-            ),
-          }}
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={formData.lastName}
+          onChange={handleInputChange}
+          className="w-full px-4 py-2 mb-4 text-[#282728] placeholder:text-sm bg-[#edefef] rounded-[21px] focus:outline-none text-sm font-normal placeholder:text-[#282728]"
         />
-      </div>
-      <button
-        type="submit"
-        className="w-full px-4 py-2 text-[#ffffff] transition-colors duration-300 bg-[#283275] rounded-[21px] text-sm font-semibold"
-      >
-        SEND INVITE
-      </button>
-    </form>
-  </div>  )
+
+        <div className="mb-4">
+          <Select
+            options={roles}
+            placeholder="Role"
+            value={formData.role}
+            onChange={(selectedOption) => handleChange(selectedOption, 'role')}
+            styles={customSelectStyles}
+          />
+        </div>
+
+        <div className="mb-4">
+          <Select
+            options={chapters}
+            placeholder="Chapter"
+            value={formData.chapter}
+            onChange={(selectedOption) => handleChange(selectedOption, 'chapter')}
+            styles={customSelectStyles}
+          />
+        </div>
+
+        <div className="mb-4">
+          <Select
+            options={committees}
+            placeholder="Committee"
+            value={formData.committee}
+            onChange={(selectedOption) => handleChange(selectedOption, 'committee')}
+            styles={customSelectStyles}
+          />
+        </div>
+        
+        <button
+          type="submit"
+          className="w-full px-4 py-2 text-[#ffffff] transition-colors duration-300 bg-[#283275] rounded-[21px] text-sm font-semibold"
+        >
+          SEND INVITE
+        </button>
+      </form>
+    </div>)
 }
 
 export default AddMemberForm
