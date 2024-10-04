d2035382545ee74a197e01c559833033d50ccde4
saurabh-vardaam <srathva@vardaam.email>
2024-10-04 20:25:30 +0530

    password type added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -45,6 +45,7 @@ const Login = () => {
           className="border "
         />
         <TextInput
+        type="password"
           value={data?.password}
           onChange={(e) =>
             setData((pre) => ({ ...pre, password: e.target.value }))
